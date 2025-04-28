9d13ff3285b961efc9c48d3835ecba0853051b32
Davi Santos <daviir17@gmail.com>
2025-04-28 01:44:29 -0300

    inicialização

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+export default function AboutPage() {
+  return (
+    <h1>Olá, mundo do about</h1>
+  );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "The blog",
+  description: "Essa é a descrição do site",
+};
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
+  return (
+    <html lang="en">
+      <body>
+        <header>header</header>
+        <div className="bg-red-500">{children}</div>
+        <footer>footer</footer>
+      </body>
+    </html>
+  );
+}
