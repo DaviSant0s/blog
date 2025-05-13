5f7105df25fb5b862ec98de58586217d4ebcd8c8
Davi Santos <daviir17@gmail.com>
2025-05-13 01:28:03 -0300

    colocando a imagem em grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,41 @@
 import Container from '@/components/Container';
+import Header from '@/components/Header';
 import PostList from '@/components/PostList';
 import { SpinLoader } from '@/components/SpinLoader';
+import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
     <Container>
+      <Header />
 
-      <header>
-        <h1 className="text-6xl font-bold text-center py-8">Aqui é o Header</h1>
-      </header>
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
+          <Image
+            className="group-hover:scale-105 transition"
+            src="/images/bryen_0.png"
+            width={1200}
+            height={720}
+            alt=""
+          />
+        </Link>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          repellat reprehenderit iste veniam sapiente incidunt ex a impedit
+          cupiditate voluptatem atque, fugit, odio alias, maiores beatae
+          distinctio accusantium? Magni, doloribus.
+        </div>
+      </section>
+
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>
+
       <footer>
         <h1 className="text-6xl font-bold text-center py-8">Aqui é o Footer</h1>
       </footer>
-
     </Container>
   );
 }
